aa9116b3ca12b2d864bbd944ecb5cbed86d480c8
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2026-04-11 15:40:27 +0200

    Adding Github actions1

--- app/database/db_connection.js
+++ app/database/db_connection.js
@@ -29,8 +29,9 @@ const { Pool } = require("pg");
 
 // const db = new Pool(config[env]);
 
+console.log(process.env.DATABASE_URL);
 console.log(
-  `--------------------------------------------------------${process.env.DATABASE_URL}----------------------------------------------------`,
+  `---------------------------------${process.env.DATABASE_URL}----------------------------------------------------`,
 );
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
