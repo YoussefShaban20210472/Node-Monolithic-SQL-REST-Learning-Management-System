aef06d16e645686ff9c8e6b61c5762c8e7ab6979
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2026-04-12 23:42:04 +0200

    Removing console.logs3

--- app/middleware/errorMiddleware.js
+++ app/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 const handleAppError = require("../error/appError");
 function errorAppHandler(error, req, res, next) {
-  console.log(error);
   const result = handleAppError(error);
+  if (result.status == 500) console.log(result);
   res.status(result.status).json({ errors: result.errors });
 }
 
