0bdeba09bca3e0f11d3f81953cb7e18e6944579b
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2026-04-21 18:56:29 +0200

    Adding enrollments apis and their tests

--- test/integration/enrollment.integration.test.js
+++ test/integration/enrollment.integration.test.js
@@ -160,7 +160,7 @@ describe("Testing Post /enrollment", () => {
         .post(`/course/${courseId}/enrollment`)
         .set("Authorization", `Bearer ${studentToken}`)
         .send();
-      console.log(studentToken, studentId, courseId);
+      console.log(studentToken, studentId, courseId, response.bod);
       expect(response.status).toBe(201);
     });
 
@@ -169,7 +169,7 @@ describe("Testing Post /enrollment", () => {
         .post(`/course/${courseId}/enrollment`)
         .set("Authorization", `Bearer ${adminToken}`)
         .send({ student_id: `${studentId}` });
-      console.log(adminToken, studentId, courseId);
+      console.log(adminToken, studentId, courseId, response.body);
       expect(response.status).toBe(201);
     });
   });
